8fe01f8411946a7fdfe978b0f585d7446f8e163d
junior <junior@PC-JUNIOR>
2023-12-06 00:28:39 -0300

    Adicionar arquivos de projeto.

---
+++ progamação trabalho/Form.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloWorld
+{
+    public class Form
+    {
+
+        private object MessageBox(string v) => throw new NotImplementedException();
+    }
+}
---
+++ progamação trabalho/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public partial class Form1: Form
+    {
+        public object MessageBox { get; private set; }
+
+#pragma warning disable CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
+        public Form1()
+#pragma warning restore CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e) => _ = ("Olá, Mundo!");
+    }
+}
